68e3ff035a41a2e68d5ae8e6a3a7cc7cd9bb0229
HUANGJIEHUI <847884008@qq.com>
2020-07-17 20:28:28 +0800

    swiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper";
+
   import {getHomeMultidata} from "network/home";
+
 
   export default {
     name: "home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
     },
     data() {
       return {
