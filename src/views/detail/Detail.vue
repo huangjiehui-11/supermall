869b4b971ec0b82718b7739d6d9a85a7d48f5ade
HUANGJIEHUI <847884008@qq.com>
2020-07-23 20:27:54 +0800

    Package DetailNavBar

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>{{iid}}</div>
+  <div id="detail">
+    <detail-nav-bar/>
+  </div>
 </template>
 
 <script>
+  import DetailNavBar from "./childComps/DetailNavBar";
+
   export default {
     name: "Detail",
+    components: {
+      DetailNavBar
+    },
     data() {
       return {
         iid: null
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav-bar>
+      <div slot="left" class="back" @click="backClick">
+        <img src="~assets/images/common/back.svg" alt="">
+      </div>
+      <div slot="center" class="title">
+        <div v-for="(item, index) in titles"
+             class="title-item"
+             :class="{active: index === currentIndex}"
+             @click="titleClick(index)">
+          <span>{{item}}</span>
+        </div>
+      </div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+
+  export default {
+    name: "DetailNavBar",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        titles: ['商品', '参数', '评论', '推荐'],
+        currentIndex: 0
+      }
+    },
+    methods: {
+      titleClick(index) {
+        this.currentIndex = index
+      },
+      backClick() {
+        this.$router.go(-1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    display: flex;
+  }
+
+  .title-item {
+    flex: 1;
+  }
+
+  .active {
+    color: var(--color-high-text);
+    border-bottom: 2px solid var(--color-tint);
+  }
+
+  .back img {
+    margin-top: 11px;
+  }
+
+
+</style>
