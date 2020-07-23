d745a9a6fc7e46f1b924d75e417a93cb67a1d67b
HUANGJIEHUI <847884008@qq.com>
2020-07-23 18:11:35 +0800

    Home.vue route to Detail.vue

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="goods-item">
+  <div class="goods-item" @click="itemClick">
     <!--load属性是用来监听图片的加载-->
     <img :src="goodsItem.show.img" alt="" @load="imageLoad">
     <div class="goods-info">
@@ -24,6 +24,9 @@
     methods: {
       imageLoad() {
         this.$bus.$emit('itemImageLoad')
+      },
+      itemClick() {
+        this.$router.push('/detail/' + this.goodsItem.iid)
       }
     }
   }
