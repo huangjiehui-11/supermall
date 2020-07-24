a5146d17c701329b87fe53053920537d49ef6997
HUANGJIEHUI <847884008@qq.com>
2020-07-24 22:04:37 +0800

    Show GoodsRecommends Information

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="goods-item" @click="itemClick">
     <!--load属性是用来监听图片的加载-->
-    <img :src="goodsItem.show.img" alt="" @load="imageLoad">
+    <img :src="showImage" alt="" @load="imageLoad">
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
       <span class="price">{{goodsItem.price}}</span>
@@ -19,6 +19,11 @@
         default() {
           return {}
         }
+      }
+    },
+    computed: {
+      showImage() {
+        return this.goodsItem.image || this.goodsItem.show.img
       }
     },
     methods: {
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -8,6 +8,7 @@
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
       <detail-param-info :param-info="paramInfo"/>
       <detail-comment-info :comment-info="commentInfo"/>
+      <goods-list :goods="recommends"/>
     </scroll>
   </div>
 </template>
@@ -20,10 +21,11 @@
   import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
   import DetailParamInfo from "./childComps/DetailParamInfo";
   import DetailCommentInfo from "./childComps/DetailCommentInfo";
+  import GoodsList from "components/content/goods/GoodsList";
 
   import Scroll from "components/common/scroll/Scroll";
 
-  import { getDetail, Goods, Shop, GoodsParam } from "network/detail";
+  import { getDetail, Goods, Shop, GoodsParam, getRecommend } from "network/detail";
 
   export default {
     name: "Detail",
@@ -36,7 +38,8 @@
       DetailParamInfo,
       DetailCommentInfo,
       Scroll,
-      GoodsParam
+      GoodsParam,
+      GoodsList
     },
     data() {
       return {
@@ -46,7 +49,8 @@
         shop: {},
         detailInfo: {},
         paramInfo: {},
-        commentInfo: {}
+        commentInfo: {},
+        recommends: []
       }
     },
     created() {
@@ -78,6 +82,12 @@
         if (data.rate.cRate !== 0) {
           this.commentInfo = data.rate.list[0]
         }
+      })
+
+      // 3.请求推荐数据
+      getRecommend().then(res => {
+        // console.log(res);
+        this.recommends = res.data.list
       })
     },
     methods: {
