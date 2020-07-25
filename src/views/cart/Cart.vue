cf050e3b325eb55069b4e0cbc9021a26777cd515
HUANGJIEHUI <847884008@qq.com>
2020-07-25 21:05:42 +0800

    NavBar in Cart.vue

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,13 +1,44 @@
 <template>
-  <h2>购物车</h2>
+  <div id="cart">
+    <nav-bar class="nav-bar"><div slot="center">购物车({{length}})</div></nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
+
+  //mapGetters辅助函数仅仅是将store中的getter映射到局部计算属性
+  import { mapGetters } from 'vuex'
+
   export default {
-    name: "cart"
+    name: "Cart",
+    components: {
+      NavBar,
+    },
+    computed: {
+      //两种语法
+      // 1.数组语法
+      // ...mapGetters(['cartLength', 'cartList'])
+      // 2.对象语法
+      ...mapGetters({
+        length: 'cartLength',
+        list: 'cartList'
+      })
+    }
   }
 </script>
 
 <style scoped>
+  #cart {
+    /*position: relative;*/
+    height: 100vh;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
 
 </style>
+
